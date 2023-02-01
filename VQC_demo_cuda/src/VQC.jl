8d27cfdb54a773c3dafd950b3026e871f112088a
LeeQY1996 <lqy859405@gmail.com>
2023-02-01 18:54:06 +0800

    Add files via upload

---
+++ VQC_demo_cuda/src/VQC.jl
@@ -0,0 +1,93 @@
+module VQC
+
+
+using Zygote
+using Zygote: @adjoint
+using CUDA
+using CUDA: @cuda
+
+using LinearAlgebra, StaticArrays, QuantumCircuits, QuantumCircuits.Gates
+using QuantumCircuits: permute
+import LinearAlgebra, QuantumCircuits
+
+# using KrylovKit: exponentiate
+# using SparseArrays: spzeros, sparse, SparseMatrixCSC
+# using Logging: @warn
+
+
+
+
+# statevector
+export StateVector, DensityMatrix, distance, distance2, onehot_encoding, qubit_encoding, reset!, amplitude, amplitudes, StateVectorBatch, DensityMatrixBatch, nitems, CuStateVectorBatch, CuDensityMatrixBatch
+
+export tr, dot, norm, normalize!, normalize, ishermitian
+export reset_qubit!, reset_onehot!, storage, fidelity, rand_state, rand_densitymatrix, permute
+export schmidt_numbers, renyi_entropy
+
+# gate operations
+export apply!
+
+# measurement
+export measure, measure!
+
+
+# hamiltonian
+export expectation
+
+# AD for post selection may be removed in the future
+export post_select, post_select!
+
+# partial trace
+export partial_tr
+
+# utility functions
+
+
+
+# auxiliary
+include("auxiliary/distance.jl")
+include("auxiliary/parallel_for.jl")
+include("auxiliary/sampling.jl")
+include("auxiliary/tensorops.jl")
+include("auxiliary/indexop.jl")
+
+# definitions of pure quantum state
+include("statevector.jl")
+include("statevectorbatch.jl")
+include("custatevectorbatch.jl")
+
+# definitions of pure quantum state batch (mainly used in quantum machine learning; maybe extend some function in the future.)
+
+# density matrix representation 
+include("densitymatrix.jl")
+include("densitymatrixbatch.jl")
+include("cudensitymatrixbatch.jl")
+
+# quantum gate operations
+include("applygates/applygates.jl")
+
+
+# measurement and postselection
+include("measure.jl")
+include("postselect.jl")
+
+# hamiltonian expectation
+include("hamiltonian/util.jl")
+include("hamiltonian/apply_qterms/apply_qterms.jl")
+include("hamiltonian/expecs/expecs.jl")
+include("hamiltonian/expecs_dm/expecs_dm.jl")
+
+
+# differentiation
+include("circuitdiff.jl")
+include("expecdiff.jl")
+include("additional_adjoints.jl")
+
+# partial trace
+include("ptrace.jl")
+
+# utility functions
+include("utility/utility.jl")
+
+
+end
